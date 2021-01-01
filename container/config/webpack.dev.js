5e13a1683c79335ddd6223dbbb8904ba4a4660d6
JKorum <constantine.proshin@gmail.com>
2021-01-01 18:44:10 +0300

    alter configs & add container action config

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
@@ -17,9 +17,6 @@ const devConfig = {
       name: 'container',
       remotes: { marketing: 'marketing@http://0.0.0.0:8081/remoteEntry.js' },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
   // output: {
